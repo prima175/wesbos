fed1155d922aece71ee50b57ab92d9aa43be421e
Prima <primadhar175@gmail.com>
2022-03-24 18:30:26 +0600

    shopping cart development done along with search component

--- Advanced-React/sick-fits/backend/mutations/addtoCart.ts
+++ Advanced-React/sick-fits/backend/mutations/addtoCart.ts
@@ -1,9 +1,46 @@
+/* eslint-disable */
 import { keystoneContext } from '@keystone-next/types';
 
-export default function addToCart(
+import { CartItemCreateInput } from '../.keystone/schema-types';
+import { Session } from '../types';
+
+async function addToCart(
   root: any,
   { productId }: { productId: string },
   context: keystoneContext
-) {
-  console.log('hiiii');
+): Promise<CartItemCreateInput> {
+  console.log('already in cart');
+  // query the current user see if they are signed in
+  const sesh = context.session as Session;
+  if (!sesh.itemId) {
+    throw new Error('you  must be logged in');
+  }
+  // query the current user's cart
+  const allCartItems = await context.lists.CartItem.findMany({
+    where: { user: { id: sesh.itemId }, product: { id: productId } },
+    resolveFields: 'id,quantity',
+  });
+  // console.log(allCartItems);
+
+  const [existingCartItem] = allCartItems;
+  if (existingCartItem) {
+    console.log(existingCartItem);
+
+    console.log(
+      `there are already ${existingCartItem.quantity}, incremented by 1`
+    );
+    // if it is iccrement by 1
+    return await context.lists.CartItem.updateOne({
+      id: existingCartItem.id,
+      data: { quantity: existingCartItem.quantity + 1 },
+    });
+  }
+  // create a new cart item
+  return await context.lists.CartItem.createOne({
+    data: {
+      product: { connect: { id: productId } },
+      user: { connect: { id: sesh.itemId } },
+    },
+  });
 }
+export default addToCart;
